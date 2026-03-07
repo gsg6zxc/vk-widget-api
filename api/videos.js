332e53bf807d05330d5f250b0a88a5655faf45d6
gsg6 <140069343+gsg6zxc@users.noreply.github.com>
2026-03-07 16:13:03 +0400

    Update videos.js

--- api/videos.js
+++ api/videos.js
@@ -1,13 +1,10 @@
-const token = 'VK_TOKEN';
-const url = `https://api.vk.com/method/account.getAppPermissions?access_token=${token}&v=5.131`;
+export default async function handler(req, res) {
+  const token = process.env.VK_TOKEN
 
-fetch(url)
-  .then(response => response.json())
-  .then(data => {
-    if (data.response) {
-      console.log('Разрешения токена:', data.response);
-    } else {
-      console.error('Ошибка:', data.error);
-    }
-  })
-  .catch(error => console.error('Ошибка запроса:', error));
+  const url = `https://api.vk.com/method/account.getAppPermissions?access_token=${token}&v=5.131`
+
+  const response = await fetch(url)
+  const data = await response.json()
+
+  res.status(200).json(data)
+}
